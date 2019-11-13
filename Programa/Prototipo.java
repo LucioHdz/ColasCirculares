e61b982f8a5af567110a44b84917bc60a94fbf7f
LucioHdz <57549359+LucioHdz@users.noreply.github.com>
2019-11-12 23:42:36 -0600

    Update Prototipo.java

--- Programa/Prototipo.java
+++ Programa/Prototipo.java
@@ -1,5 +1,6 @@
 public class Prototipo{
   int maxTam = 5;
+  int frente,fin;
   private int siguiente(int dato){
     return (dato+1)%maxTam;
   }
@@ -9,5 +10,10 @@ public class Prototipo{
   public quitar(){
   
   }
-  
+  private boolean vacia(){
+    return frente ==siguiente(fin);
+  }
+  private boolean llena(){
+    return frente==siguiente(siguiente(fin));
+  }
 }
