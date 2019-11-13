450377949004708305c7fceb09d50a4afdc496dc
LucioHdz <57549359+LucioHdz@users.noreply.github.com>
2019-11-12 23:51:01 -0600

    Update Prototipo.java

--- Programa/Prototipo.java
+++ Programa/Prototipo.java
@@ -1,10 +1,14 @@
 public class Prototipo{
   int maxTam = 5;
   int frente,fin;
+  String cola[];
+  public Prototipo(){
+    cols = new String [5];
+  }
   private int siguiente(int dato){
     return (dato+1)%maxTam;
   }
-  public insertar(){
+  public insertar(String productos){
     
   }
   public quitar(){
